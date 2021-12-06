ba5419e4a0a3d1e5d99f987f5249885c9172bb71
keerti <sharmakeerti2302@gmail.com>
2021-12-06 11:03:26 +0530

    Add files via upload

---
+++ day1.R
@@ -0,0 +1,124 @@
+#character datatype
+total="excellent score"
+print(total)
+
+#calculator
+2+2
+sqrt(25)
+2**4*5
+pi
+a<-10
+b<-30
+c=a+b
+print(c)
+
+#assignment and multiple line comment
+f<-2
+d<-4
+e=f+d
+print(e)
+#syntax of multiline comment
+{" entering multiple line commments
+fkheuifhehf
+erkgheuighe5ih
+erkghiehg5i"
+}
+#logical operator
+k=6
+if(k<10)
+  print("hello world")
+#check the datatypes
+v1=TRUE
+print(class(v1)) 
+
+v2=384930.5723867
+print(class(v2))
+
+
+v3=5+6i
+print(class(v3))
+
+
+#how to remove variables
+print(ls())
+rm(list-ls())
+print(ls())
+
+#vector calculation
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k1+k2)
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k1-k2)
+
+
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k1*k2)
+
+
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k1/k2)
+
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2%%k1)
+
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k1%%k2)
+
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2^k1)
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k1%%k2)
+
+#logical operators
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2==k1)
+
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2<=k1)
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2>=k1)
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2!=k1)
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2<k1)
+
+k1=c(2,3,4,5)
+k2=c(6,7,8,9)
+print(k2>k1)
+
+#print series
+k1=1:10
+k1
+
+
+
+
+
+
+
