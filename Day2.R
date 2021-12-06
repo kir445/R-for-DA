40b7220223376074f50c4c66a75c46b73e4f9add
keerti <sharmakeerti2302@gmail.com>
2021-12-06 13:48:10 +0530

    Update Day2.R

--- Day2.R
+++ Day2.R
@@ -73,6 +73,95 @@ p2=c(2,3)
 l=p1+p2
 l
 
+#vector Manipulation
+a=c(1,2,3,4,5,6)
+a++# adds a+a 
+a
+
+b=a+1#adds a+1
+b
+
+mean(a)
+sum(a)
+
+
+#appending two different vectors
+c(a,k)
+
+#indexing
+k=c("k","e","e","r","t","r","i")
+k[3]
+k[-3]
+
+#indexing individually
+k=c("k","e","e","r","t","r","i")
+k[c(0,-3)]
+v=k[c(TRUE,FALSE,FALSE,TRUE)]
+
+print(v)
+
+
+#striding using by
+print(seq(1,10,by=0.5))
+print(seq(1,20,by=--0.5))
+
+
+
+#String Functions
+#valid string
+capsicum=c("red",'green',"yellow")
+print(capsicum)
+print(class(capsicum))
+
+a='start this session '
+a
+b='"Start this session"'
+b
+
+#invalid string
+{#a='start this' string '
+  a}
+
+h="Hello"
+H="How"
+A="Are"
+U="You?"
+
+Greet=paste(h,H,A,U)
+Greet
+
+
+#substring out of a String
+substr("This is Data Analytics Class",start=3,stop = 15)
+data1=substring("Analytics",3,5)
+data1
+
+
+#length of characters
+result=nchar("This i a Data Analytics Class")
+result
+len=length(c("This","Is","Data","Analytics","class"))
+len
+
+
+#to upper case and lower case
+d1='This Is a Class'
+tolower(d1)
+toupper((d1))
+
+#Lists in R
+
+mylist=list("red","green",c(1,2,3),TRUE,45,78,56,4)
+print(mylist)
+
+
+
+
+
+
+
+
+
 
 
 
